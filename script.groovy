b4e9ae0ae61fc107b6cfde4bb0c7e0bedcac7dab
YarvaSuma <103576197+YarvaSuma@users.noreply.github.com>
2022-05-18 09:59:37 +0530

    groovy script

---
+++ script.groovy
@@ -0,0 +1,23 @@
+def buildApp(){
+    echo " Build Stage"
+  echo "Deploying version ${params.test}"
+    echo " Building version ${NEW_VERSION}"
+}
+def testApp(){
+     echo " test Stage"
+}
+def deployApp(){
+    echo "Deploying version ${params.VERSION}"
+                // echo " Branch name ${env.BRANCH_NAME}"
+    withCredentials([usernamePassword(credentialsId: 'github_cred', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh 'echo $PASSWORD'
+        // also available as a Groovy variable
+        echo USERNAME
+        // or inside double quotes for string interpolation
+        echo "username is $USERNAME"
+    }
+               
+    echo " Deploy Stage"
+}
+
+return this
